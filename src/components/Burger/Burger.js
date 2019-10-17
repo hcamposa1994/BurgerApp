223bdb95e539b2030c7397185b77c7c5788e78de
Herbert Campos <wohoforever@yahoo.com>
2019-10-16 20:47:23 -0700

    added in a way to dynamically add in ingredients using state from burger builder stateful component, and using a transformer to be able to convert an object into an array so map would work

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,15 @@ import classStyles from "./Burger.module.css";
 import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, index) => {
+      return <BurgerIngredient key={igKey + index} type={igKey} />;
+    });
+  });
   return (
     <div className={classStyles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );

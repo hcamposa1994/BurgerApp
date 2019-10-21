6ae7f2e225060af6f62e17ce129fc0a063937c9b
Herbert Campos <wohoforever@yahoo.com>
2019-10-20 21:27:11 -0700

    added in an order button on the burger Controls component. Added in styling to said button as well to change depending on both if cursor is hovering and disabled. Added a new state and handler on burger Builder container to be able to update when an ingredient is chosen, the order button is enabled, but when there are no ingredients, the order button is disabled

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,6 +26,9 @@ const BuildControls = props => {
           disabled={props.disabled[ctrl.type]}
         />
       ))}
+      <button className={classStyles.OrderButton} disabled={!props.purchasable}>
+        ORDER NOW
+      </button>
     </div>
   );
 };

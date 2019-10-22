698357dd9ead834e5316474b04dbe1e12041cb6c
Herbert Campos <wohoforever@yahoo.com>
2019-10-21 20:49:42 -0700

    added in a modal component to be able to display the Order Summary component as a Modal. Also added in a backdrop component, but for the time being it is empty. Created the Order Summary component to output a summary of the current order, though for the moment you cant get rid of it

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igkey => {
+    return (
+      <li key={igkey}>
+        <span style={{ textTransform: "capitalize" }}>{igkey}</span>:{" "}
+        {props.ingredients[igkey]}
+      </li>
+    );
+  });
+
+  return (
+    <Fragment>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to Checkout?</p>
+    </Fragment>
+  );
+};
+
+export default OrderSummary;

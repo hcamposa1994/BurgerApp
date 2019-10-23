e32e01d33916fc6445ccb8d3752449d7f6ffbc29
Herbert Campos <wohoforever@yahoo.com>
2019-10-22 18:40:23 -0700

    implemented the backdrop component so that if we can close the modal upon clicking the backdrop if needed

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import classStyles from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
 
 const Modal = props => {
   return (
-    <div
-      className={classStyles.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0"
-      }}
-    >
-      {props.children}
-    </div>
+    <Fragment>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classStyles.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0"
+        }}
+      >
+        {props.children}
+      </div>
+    </Fragment>
   );
 };
 

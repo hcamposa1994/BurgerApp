e0c4999090c517007955a9e1e85dd66846ff13a6
Herbert Campos <wohoforever@yahoo.com>
2019-10-24 23:29:50 -0700

    added in some optimization such as order summary not updating when we put in a new ingredient, as its unessasary to update until we show the modal. Also restructured file placement

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,34 +1,41 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igkey => {
-    return (
-      <li key={igkey}>
-        <span style={{ textTransform: "capitalize" }}>{igkey}</span>:{" "}
-        {props.ingredients[igkey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  // this could be a functional component, doesn't have to be a class
+  componentDidUpdate() {
+    console.log("[OrderSummary] Did Update");
+  }
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(igkey => {
+      return (
+        <li key={igkey}>
+          <span style={{ textTransform: "capitalize" }}>{igkey}</span>:{" "}
+          {this.props.ingredients[igkey]}
+        </li>
+      );
+    });
+
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
 export default OrderSummary;

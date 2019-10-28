5d8aa4611ed8359263e6c79c668f1095c1608e57
Herbert Campos <wohoforever@yahoo.com>
2019-10-27 17:26:20 -0700

    implmented a loading spinner that is rendered in the modal upon pressing the continue button. It displays anytime we are making a post request to firebase, but once its complete to modal closes itself again

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classStyles from "./Spinner.module.css";
+
+const Spinner = () => {
+  return <div className={classStyles.Loader}>Loading...</div>;
+};
+
+export default Spinner;

9091d34ca063fd9066630c3bf3aa446aead0efc4
Herbert Campos <wohoforever@yahoo.com>
2020-04-01 16:36:11 -0700

    edited BurgerIngredient, OrderSummary, and Modal Components to become functional components that Use React Hooks and used React Memo on the Modal to both make up for the loss of shouldComponentUpdate and to optimize the modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,34 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment } from "react";
 
 import Button from "../../UI/Button/Button";
 
-class OrderSummary extends Component {
-  render() {
-    const ingredientSummary = Object.keys(this.props.ingredients).map(igkey => {
-      return (
-        <li key={igkey}>
-          <span style={{ textTransform: "capitalize" }}>{igkey}</span>:{" "}
-          {this.props.ingredients[igkey]}
-        </li>
-      );
-    });
-
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(igkey => {
     return (
-      <Fragment>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType="Success" clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </Fragment>
+      <li key={igkey}>
+        <span style={{ textTransform: "capitalize" }}>{igkey}</span>:{" "}
+        {props.ingredients[igkey]}
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <Fragment>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Fragment>
+  );
+};
 
 export default OrderSummary;
